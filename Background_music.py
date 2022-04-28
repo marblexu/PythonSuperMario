46756b3a2c4734860dd39a3b4c5c0adb15a56a09
CharlesHit <charles.m.hau@gmail.com>
2022-04-28 10:56:00 -0400

    Fixed SM13: Music Added

--- Background_music.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from pygame import mixer
-
-pygame.init()
-mixer.music.load("Little-West-Finaly.ogg")
-mixer.music.play(-1)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame as pg
 from source.main import main
+from pygame import mixer
 
 if __name__=='__main__':
+    mixer.init()
+    mixer.music.load("Little-West-Finaly.ogg")
+    mixer.music.play()
     main()
     pg.quit()
